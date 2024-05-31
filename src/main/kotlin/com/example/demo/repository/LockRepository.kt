c72a019d054335e162750cdec79fdc2f55912bca
SlimskiTheWise <hun245600@gmail.com>
2024-05-31 18:52:33 +0900

    fix: timeout 3000 -> 3

--- src/main/kotlin/com/example/demo/repository/LockRepository.kt
+++ src/main/kotlin/com/example/demo/repository/LockRepository.kt
@@ -3,13 +3,16 @@ package com.example.demo.repository
 import com.example.demo.domain.Stock
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
 
+@Repository
 interface LockRepository : JpaRepository<Stock, Long> {
-    @Query(value = "select get_lock(:key, 3000)", nativeQuery = true)
-    fun getLock(key: String?)
+    @Query("SELECT GET_LOCK(:key, 3)", nativeQuery = true)
+    fun getLock(@Param("key") key: String): Int?
 
-    @Query(value = "select release_lock(:key)", nativeQuery = true)
-    fun releaseLock(key: String?)
+    @Query("SELECT RELEASE_LOCK(:key)", nativeQuery = true)
+    fun releaseLock(@Param("key") key: String): Int?
 }
 
 
