cc63c24a12f66ac7b8744ed67a19446b7a05e5d0
SlimskiTheWise <hun245600@gmail.com>
2024-05-31 18:53:07 +0900

    fix: add propagation option

--- src/main/kotlin/com/example/demo/service/StockService.kt
+++ src/main/kotlin/com/example/demo/service/StockService.kt
@@ -2,8 +2,9 @@ package com.example.demo.service
 
 import com.example.demo.domain.Stock
 import com.example.demo.repository.StockRepository
-import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.annotation.Propagation
 
 @Service
 class StockService (val stockRepository: StockRepository): IStockService {
@@ -13,5 +14,12 @@ class StockService (val stockRepository: StockRepository): IStockService {
             stock.decreaseQuantity(quantity)
             stockRepository.saveAndFlush(stock)
         }
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    fun decreaseForNamedLocking(id: Long, quantity: Long) {
+        val stock: Stock = stockRepository.findById(id).orElseThrow()
+        stock.decreaseQuantity(quantity)
+        stockRepository.saveAndFlush(stock)
     }
 }
